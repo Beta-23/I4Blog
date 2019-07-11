679305b7f1aa207ba9e2ac85014923868e3cad89
Beta-23 <alfred.dominguez@gmail.com>
2019-07-11 10:59:23 -0700

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
   resources :blogs
 
   root to: 'pages#home'
+  # Custom route method
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'  
 end
