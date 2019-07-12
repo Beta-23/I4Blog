d3779233e4d2ced30309eb90fd203ee1b3c347a0
Beta-23 <alfred.dominguez@gmail.com>
2019-07-11 18:59:18 -0700

    Update status for enum

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,6 @@
 class Blog < ApplicationRecord
+  # Data Query scope and method for blog post status
+  enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
