c408374e591fb0ac918f847a29dc312f0781014b
Beta-23 <alfred.dominguez@gmail.com>
2019-07-15 15:10:09 -0700

    Integrated database relationship between topics and blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog < ApplicationRecord
   # Data Query scope and method for blog post status
+  belongs_to :topic
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
