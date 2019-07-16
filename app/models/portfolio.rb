8a2a16be925ffd187cc862018504839e81327b13
Beta-23 <alfred.dominguez@gmail.com>
2019-07-16 12:04:48 -0700

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+    #Validations
     validates_presence_of :title, :body, :main_image, :thumb_image
 
     # Custom scope for angular database query
@@ -7,4 +8,13 @@ class Portfolio < ApplicationRecord
     end
     # Creating a lambda using shorthand notation
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+    #Call back at model level
+    after_initialize :set_defaults
+
+    #Method to ensure images are not nil after the @portfolio create call back at portfolio_controller action
+    def set_defaults
+      self.main_image ||= "https://via.placeholder.com/600x400"
+      self.thumb_image ||= "https://via.placeholder.com/350x200" 
+    end
 end
