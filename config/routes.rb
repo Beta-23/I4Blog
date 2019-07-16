71ee78bf5c7b95c5e943aac308063b30b281c7d3
Beta-23 <alfred.dominguez@gmail.com>
2019-07-16 11:03:23 -0700

    Constructed custom scopes for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   # Custom route method
+  get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
-  get 'contact', to: 'pages#contact'  
+  get 'contact', to: 'pages#contact' 
 end
