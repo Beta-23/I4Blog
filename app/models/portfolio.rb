c42d98e4e46857ae12578b1f22a329ed7a421cf8
Beta-23 <alfred.dominguez@gmail.com>
2019-07-16 22:11:47 -0700

    Integrated technologies model and update seed with new data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,21 +1,22 @@
 class Portfolio < ApplicationRecord
+  has_many :technologies
   include Placeholder
-    #Validations
-    validates_presence_of :title, :body, :main_image, :thumb_image
+  #Validations
+  validates_presence_of :title, :body, :main_image, :thumb_image
 
-    # Custom scope for angular database query
-    def self.angular
-      where(subtitle: 'Angular')
-    end
-    # Creating a lambda using shorthand notation
-    scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+  # Custom scope for angular database query
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+  # Creating a lambda using shorthand notation
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
-    #Call back at model level
-    after_initialize :set_defaults
+  #Call back at model level
+  after_initialize :set_defaults
 
     #Method to ensure images are not nil after the @portfolio create call back at portfolio_controller action, it also calls the placeholder module (concerns)
-    def set_defaults
-      self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-      self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-    end
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -50,6 +50,15 @@ end
 
 puts "9 portfolio_items created"
 
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology#{technology}",
+)
+end
+
+puts "3 technologies created"
+
+
 
 
 
