f9376ece80ebea55608e915045c3773bfcaaf6c2
Beta-23 <alfred.dominguez@gmail.com>
2019-09-10 13:16:31 -0700

    Implemented session tracker for dynamic source links

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   end
 
   root to: 'pages#home'
+  
   # Custom route method
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
